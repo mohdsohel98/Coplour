e4c4841713d5ccefb60e3d4aff93df7ef6b90a75
Mohd.Sohel  mansoori <151987846+mohdsohel98@users.noreply.github.com>
2025-05-19 00:32:15 +0530

    Delete ReactAssignment-5 directory

--- ReactAssignment-5/ReactAssignment-3/reactAssign-3/src/components/Fail.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Fail() {
-  return (
-    <span className="text-danger">fail</span>
-  )
-}
-
-export default Fail
--- ReactAssignment-5/ReactAssignment-3/reactAssign-3/src/components/Pass.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Pass() {
-  return(
-   <span className="text-success">pass</span>
-  )
-}
-
-export default Pass
